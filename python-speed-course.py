3d704b820c768dfb20fc185c5ace3589e0c7c609
Tom-Quirk <thomas.q@hotmail.co.uk>
2023-08-15 13:39:25 +0100

    python syntax speedrun

---
+++ python-speed-course.py
@@ -0,0 +1,28 @@
+# tech with tim tutorial
+
+print(True, "hello world!")
+
+hello = 'tom'
+word = hello
+
+print(word)
+
+hello_world = "hello world!"
+
+print(hello_world)
+
+
+# how to use input
+
+# name = input('Name: ')
+
+# print(name)
+
+# methods
+hello = 'hello'.upper()
+print(hello)
+print(hello.capitalize())
+print(hello.lower().count('o')) # returns number
+
+
+# conditionals (always return boolean)
